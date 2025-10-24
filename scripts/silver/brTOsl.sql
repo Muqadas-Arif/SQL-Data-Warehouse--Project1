01a7b63ed145fdb8dbc4ad46e39edfef6de9641a
Muqadas Arif <muqadasarif95@gmail.com>
2025-10-25 00:41:22 +0500

    Insert customer and product data into silver schema

--- scripts/silver/brTOsl.sql
+++ scripts/silver/brTOsl.sql
@@ -1,1 +1,82 @@
+table1
+insert into silver.crm_cust_info
+	(cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_material_status,
+	cst_gndr
+
+	)
+select 
+cst_id,
+cst_key,
+trim(cst_firstname) as cst_firstname,
+trim(cst_lastname) as cst_lastname ,
+case when upper(trim(cst_material_status))='S' then 'Single'
+     when upper(trim(cst_material_status))='M' then 'Married'
+	 else 'n/a'
+	 end cst_material_status,
+
+case when upper(trim(cst_gndr))='F' then 'Female'
+     when upper(trim(cst_gndr))='M' then 'Male'
+	 else 'n/a'
+	 end as cst_gndr
+
+from (
+select
+*,
+row_number() over(partition by cst_id order by cst_create_date desc) as flag_last
+from bronze.crm_cust_info
+where cst_id is not null
+)t where flag_last =1
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>table2>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+use datawarehouse
+insert into silver.crm_prd_info (
+prd_id,
+prd_key, 
+prd_nm, 
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt,
+cat_id          
+)
+
+select 
+	prd_id,
+	substring(prd_key,7,len(prd_key)) as prd_key,
+	prd_nm,
+	isnull(prd_cost,0) as prd_cost,
+case when upper(trim(prd_line))='M' then 'Mountain'
+	when upper(trim(prd_line))='R' then 'Road'
+	when upper(trim(prd_line))='S' then 'other Sales'
+	when upper(trim(prd_line))='T' then 'Touring'
+	else 'n/a'
+end as prd_line,
+cast(prd_start_dt as date) as prd_start_dt,
+cast(lead(prd_start_dt) over (partition by prd_key order by prd_start_dt)-1 as date) as prd_end_dt,
+replace(substring(prd_key,1,5), '-','_') as cat_id
+from bronze.crm_prd_info
+order by prd_id
+
+
+select*from bronze.crm_prd_info
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
