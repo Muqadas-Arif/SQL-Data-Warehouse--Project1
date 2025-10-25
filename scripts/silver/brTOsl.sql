730633daef2af5e8c14a1478a4d4c5b0a02183b6
Muqadas Arif <muqadasarif95@gmail.com>
2025-10-25 22:15:03 +0500

    Insert data into silver.erp_cust_az12 from bronze

--- scripts/silver/brTOsl.sql
+++ scripts/silver/brTOsl.sql
@@ -64,6 +64,7 @@ order by prd_id
 
 
 select*from bronze.crm_prd_info
+	>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>table3<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 insert  into silver.crm_sales_details(
 sls_ord_num,
@@ -107,6 +108,24 @@ else sls_price
 end as sls_price
 
 from bronze.crm_sales_details
+
+
+
+	>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>table4<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+insert into silver.erp_cust_az12(cid,bdate,gen)
+select
+case when cid like 'NAS%' then substring(cid,4,len(cid))
+else cid
+end as cid,
+case when bdate > getdate() then null
+else bdate
+end as bdate,
+case when upper(trim(gen)) in ('F','FEMALE') then 'Female'
+when upper(trim(gen)) in ('M','MALE') then 'Male'
+else 'n/a'
+end as gen
+from bronze.erp_cust_az12
 
 
 
